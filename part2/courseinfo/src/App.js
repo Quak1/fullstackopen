45b5b85a4fa4152ebc4e8099176934b65e7cddb6
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-17 10:56:25 -0700

    2.1: Course information step6

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -8,38 +8,41 @@ const Part = ({ part }) => (
   </p>
 );
 
-const Content = ({ parts }) => (
+const Content = ({ parts }) => parts.map((part) => <Part part={part} />);
+
+const Course = ({ course }) => (
   <>
-    <Part part={parts[0]} />
-    <Part part={parts[1]} />
-    <Part part={parts[2]} />
+    <Header course={course.name} />
+    <Content parts={course.parts} />
   </>
 );
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total
-        sum={parts[0].exercises + parts[1].exercises + parts[2].exercises}
-      />
+      <Course course={course} />
+      {/* <Total */}
+      {/*   sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} */}
+      {/* /> */}
     </div>
   );
 };
