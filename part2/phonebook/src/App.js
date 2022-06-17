98608c875d097bd0f54379eccaeb15e58a19aa2f
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-17 12:45:42 -0700

    2.8: The Phonebook Step3 Allow users to add phone numbers to the phone book.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,17 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (e) => {
     e.preventDefault();
     const newPerson = {
       name: newName,
+      number: newNumber,
     };
 
     const nameExists = persons.some(
@@ -19,12 +23,12 @@ const App = () => {
     }
 
     setNewName("");
+    setNewNumber("");
     setPersons(persons.concat(newPerson));
   };
 
-  const handleNameChange = (e) => {
-    setNewName(e.target.value);
-  };
+  const handleNameChange = (e) => setNewName(e.target.value);
+  const handleNumberChange = (e) => setNewNumber(e.target.value);
 
   return (
     <div>
@@ -34,12 +38,17 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <div key={person.name}>{person.name}</div>
+        <div key={person.name}>
+          {person.name} {person.number}
+        </div>
       ))}
     </div>
   );
