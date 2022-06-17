a78c1b715c43b29881045f0021c7a7ef87ad7c80
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-16 21:44:44 -0700

    1.8: unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,15 +10,31 @@ const Info = ({ text, state, tail = "" }) => (
   </div>
 );
 
+const Statistics = ({ statistics }) => {
+  const info = [];
+  for (let key in statistics)
+    info.push(<Info text={key} state={statistics[key]} />);
+  return info;
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const increaseByOne = (state, setState) => () => setState(state + 1);
+
   const total = good + neutral + bad;
   const average = (good - bad) / total;
   const positive = good / total;
+
+  const statistics = {
+    good,
+    neutral,
+    bad,
+    total,
+    average,
+  };
 
   return (
     <>
@@ -27,11 +43,7 @@ const App = () => {
       <Button onClick={increaseByOne(neutral, setNeutral)} text="neutral" />
       <Button onClick={increaseByOne(bad, setBad)} text="bad" />
       <Title title="statistics" />
-      <Info text="good" state={good} />
-      <Info text="neutral" state={neutral} />
-      <Info text="bad" state={bad} />
-      <Info text="all" state={total} />
-      <Info text="average" state={average} />
+      <Statistics statistics={statistics} />
       <Info text="positive" state={positive} tail=" %" />
     </>
   );
