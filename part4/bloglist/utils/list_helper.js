df33967d386771a91f362b46bd91cfce04f836bd
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-21 12:33:04 -0700

    4.5*: helper functions and unit tests, step3 - favoriteBlog

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -10,7 +10,20 @@ const totalLikes = (blogs) => {
       }, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  return blogs.length === 0
+    ? {}
+    : blogs.reduce((currentFavorite, blog) => {
+        if (currentFavorite.likes >= blog.likes) {
+          return currentFavorite;
+        } else {
+          return blog;
+        }
+      });
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
