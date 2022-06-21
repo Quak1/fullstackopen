2cac6c77fee2e297b30fcb9352d31cd8ec12844f
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-21 11:59:44 -0700

    4.4: helper functions and unit tests, step2

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -1,8 +1,44 @@
-const listHelper = require('../utils/list_helper')
+const listHelper = require("../utils/list_helper");
 
-test('dummy returns one', () => {
-    const blogs = []
+test("dummy returns one", () => {
+  const blogs = [];
 
-    const result = listHelper.dummy(blogs)
-    expect(result).toBe(1)
-})
+  const result = listHelper.dummy(blogs);
+  expect(result).toBe(1);
+});
+
+describe("total likes", () => {
+  const blogs = [
+    {
+      _id: "62b1fb94f86dd328a887d2ea",
+      title: "Some blog",
+      author: "An Author",
+      url: "https://www.expample.com/blog/1",
+      likes: 12,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  test("of empty list is zero", () => {
+    const result = listHelper.totalLikes([]);
+    expect(result).toBe(0);
+  });
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes([blogs[0]]);
+    expect(result).toBe(12);
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(17);
+  });
+});
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((total, blog) => {
+        return total + blog.likes;
+      }, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
