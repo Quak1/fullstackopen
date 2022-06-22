d448bf67221f4c6057854256aa19e15281d3f90f
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-22 14:47:31 -0700

    4.16*: bloglist expansion, step4 - user creation tests

--- part4/bloglist/controllers/user.js
+++ part4/bloglist/controllers/user.js
@@ -6,9 +6,27 @@ const User = require("../models/user");
 userRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
 
+  if (!username || !password) {
+    return response.status(400).json({
+      error: "username or password missing",
+    });
+  }
+  if (username.length < 3) {
+    return response.status(400).json({
+      error: "username must be at least 3 characters long",
+    });
+  }
+  if (password.length < 3) {
+    return response.status(400).json({
+      error: "password must be at least 3 characters long",
+    });
+  }
+
   const existingUser = await User.findOne({ username });
   if (existingUser) {
-    return response.status(400).json({ error: "username must be unique" });
+    return response.status(400).json({
+      error: "username must be unique",
+    });
   }
 
   const passwordHash = await bcrypt.hash(password, 10);
---
+++ part4/bloglist/tests/user_api.test.js
@@ -0,0 +1,83 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+
+const helper = require("./test_helper");
+const User = require("../models/user");
+const app = require("../app");
+const api = supertest(app);
+
+beforeEach(async () => {
+  await User.deleteMany({});
+  const singleUser = new User({
+    username: "test",
+    name: "Test Man",
+    password: "SuperPassword",
+  });
+  await singleUser.save();
+});
+
+describe("user creation", () => {
+  test("create a new user", async () => {
+    const newUser = {
+      username: "new-user",
+      name: "New User",
+      password: "new password",
+    };
+
+    const res = await api.post("/api/users").send(newUser).expect(201);
+    expect(res.body.username).toBe("new-user");
+  });
+
+  test("duplicate username gets rejected", async () => {
+    const newUser = {
+      username: "test",
+      name: "New User",
+      password: "new password",
+    };
+
+    await api.post("/api/users").send(newUser).expect(400);
+  });
+
+  test("missing username returns status code 400", async () => {
+    const newUser = {
+      username: "",
+      name: "New User",
+      password: "new password",
+    };
+
+    await api.post("/api/users").send(newUser).expect(400);
+  });
+
+  test("missing password returns status code 400", async () => {
+    const newUser = {
+      username: "new-user",
+      name: "New User",
+    };
+
+    await api.post("/api/users").send(newUser).expect(400);
+  });
+
+  test("if username is less than 3 characters long returns status code 400", async () => {
+    const newUser = {
+      username: "ne",
+      name: "New User",
+      password: "new password",
+    };
+
+    await api.post("/api/users").send(newUser).expect(400);
+  });
+
+  test("if password is less than 3 characters long returns status code 400", async () => {
+    const newUser = {
+      username: "new-user",
+      name: "New User",
+      password: "ne",
+    };
+
+    await api.post("/api/users").send(newUser).expect(400);
+  });
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
