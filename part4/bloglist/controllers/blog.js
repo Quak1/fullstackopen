a7e5b6ad609f78f1e69481974943a07ad9cda39b
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-22 15:58:11 -0700

    4.19: bloglist expansion, step7

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -1,6 +1,16 @@
+const jwt = require("jsonwebtoken");
+
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+
+const getToken = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { blogs: 0 });
@@ -8,7 +18,13 @@ blogRouter.get("/", async (request, response) => {
 });
 
 blogRouter.post("/", async (request, response) => {
-  const user = await User.findOne({});
+  const token = getToken(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
+
   const newBlog = new Blog({
     ...request.body,
     user: user._id,
