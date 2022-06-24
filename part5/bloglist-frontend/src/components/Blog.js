17f0007291ddacdaf33238524aa3262f8c79a15a
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-24 12:26:12 -0700

    5.7* Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,32 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  if (showDetails) {
+    return (
+      <div className="blogDetails">
+        <div>
+          {blog.title} by {blog.author}{" "}
+          <button onClick={() => setShowDetails(false)}>hide</button>
+        </div>
+        <div>{blog.url}</div>
+        <div>
+          likes: {blog.likes}{" "}
+          <button onClick={() => console.log("liked!")}>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {blog.title} by {blog.author}
+      <button onClick={() => setShowDetails(true)}>view</button>
+    </div>
+  );
+};
+
+export default Blog;
+
