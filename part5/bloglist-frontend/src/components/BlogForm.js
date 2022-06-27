f24ef75b3a229788e6d05474364e991177915c0a
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-27 14:48:35 -0700

    Add test for BlogForm componenet

--- part5/bloglist-frontend/src/components/BlogForm.js
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -28,6 +28,7 @@ const BlogForm = ({ createBlog }) => {
             value={title}
             name="Title"
             onChange={({ target }) => setTitle(target.value)}
+            placeholder="title"
           />
         </div>
         <div>
@@ -37,6 +38,7 @@ const BlogForm = ({ createBlog }) => {
             value={author}
             name="Author"
             onChange={({ target }) => setAuthor(target.value)}
+            placeholder="author"
           />
         </div>
         <div>
@@ -46,6 +48,7 @@ const BlogForm = ({ createBlog }) => {
             value={url}
             name="Url"
             onChange={({ target }) => setUrl(target.value)}
+            placeholder="url"
           />
         </div>
         <button type="submit">create</button>
