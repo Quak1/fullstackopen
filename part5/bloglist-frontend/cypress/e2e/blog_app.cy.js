9ec9ed6121891718c86efb0d41353eb7ad344f48
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-29 12:25:42 -0700

    5.18: bloglist end to end testing, step2 - login tests

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,11 +1,40 @@
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+    cy.createUser("Tester Name", "myUser", "password");
     cy.visit("http://localhost:3000");
   });
 
   it("Login form is shown", function () {
     cy.contains("Log in to application");
     cy.contains("login");
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.contains("username").find("input").type("myUser");
+      cy.contains("password").find("input").type("password");
+      cy.contains("login").click();
+
+      cy.contains("Tester Name logged in");
+      cy.contains("You have logged in!").should(
+        "have.css",
+        "color",
+        "rgb(0, 128, 0)"
+      );
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.contains("username").find("input").type("badUser");
+      cy.contains("password").find("input").type("password");
+      cy.contains("login").click();
+
+      cy.contains("Log in to application");
+      cy.contains("Wrong username or password").should(
+        "have.css",
+        "color",
+        "rgb(255, 0, 0)"
+      );
+    });
   });
 });
--- part5/bloglist-frontend/cypress/support/commands.js
+++ part5/bloglist-frontend/cypress/support/commands.js
@@ -10,16 +10,18 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", (username, password) => {
+  cy.request("POST", "http://localhost:3003/api/login", {
+    username,
+    password,
+  }).then(({ body }) => {
+    localStorage.setItem("loggedUser", JSON.stringify(body));
+  });
+});
+
+Cypress.Commands.add("createUser", (name, username, password) => {
+  const user = { name, username, password };
+
+  cy.request("POST", "http://localhost:3003/api/users", user);
+});
+
