5124c27066a2b2a1dc6ff50442caaa3be71399bd
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-01 11:36:27 -0700

    6.14 Anecdotes and the backend, step2

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,17 +1,19 @@
 import { useDispatch } from "react-redux";
 import { addAnecdote } from "../reducers/anecdoteReducer";
 import { changeNotification } from "../reducers/notificationReducer";
+import anecdoteService from "../services/anecdote";
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
 
-  const createAnecdote = (event) => {
+  const createAnecdote = async (event) => {
     event.preventDefault();
 
     const content = event.target.anecdote.value;
-    event.target.anecdote.value = "";
-    dispatch(addAnecdote(content));
+    const anecdote = await anecdoteService.postNew(content);
+    dispatch(addAnecdote(anecdote));
 
+    event.target.anecdote.value = "";
     dispatch(changeNotification("Added a new anecdote!"));
     setTimeout(() => {
       dispatch(changeNotification(""));
---
+++ part6/redux-anecdotes/src/services/anecdote.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001/anecdotes";
+
+const getAll = async () => {
+  const res = await axios.get(baseUrl);
+  return res.data;
+};
+
+const postNew = async (content) => {
+  const res = await axios.post(baseUrl, { content });
+  return res.data;
+};
+
+export default {
+  getAll,
+  postNew,
+};
