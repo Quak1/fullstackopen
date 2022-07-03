ca5e1f5b3f1bcc9ef73acc3ea5a11b88796b6bbd
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-02 19:16:27 -0700

    6.21 anecdotes, the grand finale

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -17,9 +17,10 @@ export const setNotification =
   (content, seconds = 5) =>
   async (dispatch, getState) => {
     dispatch(changeNotification(content));
-    setTimeout(() => {
+    const timeoutId = setTimeout(() => {
       dispatch(clearNotification());
     }, seconds * 1000);
+    clearTimeout(timeoutId - 1);
   };
 
 export const { changeNotification, clearNotification } =
