f1a716bc9d8858a882bfe1a42adf97bf1798cd84
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-07 13:33:26 -0700

    7.8: ultimate hooks

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -18,10 +18,26 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  const fetchResources = async () => {
+    try {
+      const response = await axios.get(baseUrl)
+      setResources(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
-  const create = (resource) => {
-    // ...
+  useEffect(() => {
+    fetchResources()
+  }, [])
+
+  const create = async (resource) => {
+    try {
+      const response = await axios.post(baseUrl, resource)
+      fetchResources()
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   const service = {
@@ -45,10 +61,10 @@ const App = () => {
     event.preventDefault()
     noteService.create({ content: content.value })
   }
- 
+
   const handlePersonSubmit = (event) => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    personService.create({ name: name.value, number: number.value })
   }
 
   return (
@@ -62,7 +78,7 @@ const App = () => {
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
+        name <input {...name} /> <br />
         number <input {...number} />
         <button>create</button>
       </form>
