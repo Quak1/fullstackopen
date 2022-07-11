5285a7f938b9e88e6d950bf58779fb6d68e802cb
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-11 13:45:57 -0700

    Fix typo

--- part7/bloglist/frontend/.eslintrc.js
+++ part7/bloglist/frontend/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module",
   },
-  plugins: ["react", "jest", "cyppress"],
+  plugins: ["react", "jest", "cypress"],
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
