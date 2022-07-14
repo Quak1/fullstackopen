a26e8dbc64c66742ff8e6980c710ab2a67c85979
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-14 14:30:46 -0700

    8.10: Adding a book

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -17,7 +17,27 @@ export const ALL_BOOKS = gql`
       title
       published
       author
-      genres
+      id
+    }
+  }
+`;
+
+export const ADD_BOOK = gql`
+  mutation addBook(
+    $title: String!
+    $published: Int!
+    $author: String!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      published: $published
+      author: $author
+      genres: $genres
+    ) {
+      title
+      published
+      author
       id
     }
   }
