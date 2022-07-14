4a10dafa72f748922d31a9a10b09384a94c355ea
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-14 11:28:09 -0700

    8.2: All books

--- part8/library/index.js
+++ part8/library/index.js
@@ -89,12 +89,27 @@ let books = [
 ];
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]!
+    id: ID!
+  }
+
   type Query {
+    bookCount: Int
+    authorCount: Int
+    allBooks: [Book!]!
   }
 `;
 
 const resolvers = {
-  Query: {},
+  Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
+    allBooks: () => books,
+  },
 };
 
 const server = new ApolloServer({
