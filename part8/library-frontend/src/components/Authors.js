22de0c44ddf27925bc2107e1ff4fe4603ce18913
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-14 14:03:50 -0700

    8.8: Authors view

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,8 +1,14 @@
+import { useQuery } from "@apollo/client";
+
+import { ALL_AUTHORS } from "../queries";
+
 const Authors = (props) => {
-  if (!props.show) {
-    return null
-  }
-  const authors = []
+  const response = useQuery(ALL_AUTHORS);
+
+  if (!props.show) return null;
+  if (response.loading) return null;
+
+  const authors = response.data.allAuthors;
 
   return (
     <div>
@@ -24,7 +30,7 @@ const Authors = (props) => {
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Authors
+export default Authors;
