98eb270ed77922675863ef5eb51d078ec14a8778
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-19 13:25:53 -0700

    9.11: Patientor backend, step4

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import diagnosesRouter from "./routes/diagnoses";
+import patientsRouter from "./routes/patients";
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,7 @@ app.get("/api/ping", (_req, res) => {
 });
 
 app.use("/api/diagnoses", diagnosesRouter);
+app.use("/api/patients", patientsRouter);
 
 const PORT = 3001;
 app.listen(PORT, () => {
