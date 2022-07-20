4a9524851fa320e655b12700c2ae08834f170205
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-19 17:37:10 -0700

    Add new types and data

---
+++ part9/courseinfo/src/data.tsx
@@ -0,0 +1,31 @@
+import { CoursePart } from "./types";
+
+const courseParts: CoursePart[] = [
+  {
+    name: "Fundamentals",
+    exerciseCount: 10,
+    description: "This is the leisured course part",
+    type: "normal",
+  },
+  {
+    name: "Advanced",
+    exerciseCount: 7,
+    description: "This is the harded course part",
+    type: "normal",
+  },
+  {
+    name: "Using props to pass data",
+    exerciseCount: 7,
+    groupProjectCount: 3,
+    type: "groupProject",
+  },
+  {
+    name: "Deeper type usage",
+    exerciseCount: 14,
+    description: "Confusing description",
+    exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+    type: "submission",
+  },
+];
+
+export default courseParts;
