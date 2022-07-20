e15c18403ef8d56f7ae19dc0966cd0c8e44f3b2c
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-20 16:34:31 -0700

    9.22: patientor, step7

--- part9/patientor-frontend/src/PatientPage/Entry.tsx
+++ part9/patientor-frontend/src/PatientPage/Entry.tsx
@@ -5,26 +5,77 @@ interface Props {
   entry: EntryType;
 }
 
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
 const Entry = ({ entry }: Props) => {
   const [{ diagnoses }] = useStateValue();
 
   if (Object.keys(diagnoses).length === 0) return null;
 
-  return (
-    <div key={entry.id}>
+  const Prefix = ({ icon }: { icon: string }) => (
+    <>
       <p>
-        {entry.date} <em>{entry.description}</em>
+        {entry.date}
+        <span className="material-icons">{icon}</span>
       </p>
-      <ul>
-        {entry.diagnosisCodes &&
-          entry.diagnosisCodes.map((code) => (
+      <p>
+        <em>{entry.description}</em>
+      </p>
+      <p>Diagnose by: {entry.specialist}</p>
+      {entry.diagnosisCodes && (
+        <ul>
+          {entry.diagnosisCodes.map((code) => (
             <li key={code}>
               {code} {diagnoses[code].name}
             </li>
           ))}
-      </ul>
-    </div>
+        </ul>
+      )}
+    </>
   );
+
+  const color = ["green", "yellow", "red", "black"];
+
+  switch (entry.type) {
+    case "Hospital":
+      return (
+        <div className="entry">
+          <Prefix icon="local_hospital" />
+          <p>Discharged on: {entry.discharge.date}</p>
+        </div>
+      );
+    case "HealthCheck":
+      return (
+        <div className="entry">
+          <Prefix icon="medical_services" />
+          <div
+            className="material-icons"
+            style={{ color: color[entry.healthCheckRating] }}
+          >
+            favorite
+          </div>
+        </div>
+      );
+    case "OccupationalHealthcare":
+      return (
+        <div className="entry">
+          <Prefix icon="work" />
+          <p>Employer: {entry.employerName}</p>
+          {entry.sickLeave && (
+            <p>
+              Sick leave from {entry.sickLeave.startDate} to{" "}
+              {entry.sickLeave.endDate}
+            </p>
+          )}
+        </div>
+      );
+    default:
+      return assertNever(entry);
+  }
 };
 
 export default Entry;
