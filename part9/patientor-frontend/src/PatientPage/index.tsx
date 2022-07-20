8c5bb4164a55dbcb0ec379bf57ab1ebd0d8026c2
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-20 13:33:18 -0700

    Add patient view

---
+++ part9/patientor-frontend/src/PatientPage/index.tsx
@@ -0,0 +1,54 @@
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Typography, Container } from "@material-ui/core";
+
+import { useStateValue } from "../state";
+import { apiBaseUrl } from "../constants";
+import { Patient } from "../types";
+
+const PatientPage = () => {
+  const { id } = useParams();
+  const [{ patients }, dispatch] = useStateValue();
+
+  const fetchPatient = async (id: string) => {
+    try {
+      const { data: patient } = await axios.get<Patient>(
+        `${apiBaseUrl}/patients/${id}`
+      );
+      dispatch({ type: "ADD_PATIENT", payload: patient });
+    } catch (e) {
+      return <p>Not Found</p>;
+    }
+  };
+
+  if (!id) return <p>Not Found</p>;
+  else if (!(id in patients)) {
+    void fetchPatient(id);
+    return <p>Not Found</p>;
+  }
+
+  const patient = patients[id];
+  if (!("ssn" in patient)) {
+    void fetchPatient(id);
+    return <p>Loading...</p>;
+  }
+
+  let icon = null;
+  if (patient.gender === "male") {
+    icon = <span className="material-icons">male</span>;
+  } else if (patient.gender === "female") {
+    icon = <span className="material-icons">female</span>;
+  }
+
+  return (
+    <Container>
+      <Typography variant="h4">
+        {patient.name} {icon}
+      </Typography>
+      <Typography variant="body1">ssn: {patient.ssn}</Typography>
+      <Typography variant="body1">occupation: {patient.occupation}</Typography>
+    </Container>
+  );
+};
+
+export default PatientPage;
