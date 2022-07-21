6f7d1a7880cd4e571d4bb4945623ebf1f5ae00e8
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-21 12:27:32 -0700

    9.24: patientor, step9

---
+++ part9/patientor-frontend/src/AddEntryModal/AddEntryForm.tsx
@@ -0,0 +1,123 @@
+import { Grid, Button } from "@material-ui/core";
+import { Field, Formik, Form } from "formik";
+
+import { TextField, DiagnosisSelection } from "../AddPatientModal/FormField";
+import { useStateValue } from "../state";
+import { HospitalEntry } from "../types";
+
+export type EntryFormValues = Omit<HospitalEntry, "id">;
+
+interface Props {
+  onSubmit: (values: EntryFormValues) => void;
+  onCancel: () => void;
+}
+
+export const AddEntryForm = ({ onSubmit, onCancel }: Props) => {
+  const [{ diagnoses }] = useStateValue();
+
+  return (
+    <Formik
+      initialValues={{
+        type: "Hospital",
+        description: "",
+        date: "",
+        specialist: "",
+        diagnosisCodes: undefined,
+        discharge: {
+          date: "",
+          criteria: "",
+        },
+      }}
+      onSubmit={onSubmit}
+      validate={(values) => {
+        const requiredError = "Field is required";
+        const errors: { [field: string]: string } = {};
+        if (!values.description) {
+          errors.description = requiredError;
+        }
+        if (!values.date) {
+          errors.date = requiredError;
+        }
+        if (!values.specialist) {
+          errors.specialist = requiredError;
+        }
+        if (!values.discharge.date) {
+          errors.dischargeDate = requiredError;
+        }
+        if (!values.discharge.criteria) {
+          errors.dischargeCriteria = requiredError;
+        }
+        return errors;
+      }}
+    >
+      {({ isValid, dirty, setFieldValue, setFieldTouched }) => {
+        return (
+          <Form className="form ui">
+            <Field
+              label="Description"
+              placeholder="Description"
+              name="description"
+              component={TextField}
+            />
+            <Field
+              label="Date"
+              placeholder="YYYY-MM-DD"
+              name="date"
+              component={TextField}
+            />
+            <Field
+              label="Specialist"
+              placeholder="Specialist"
+              name="specialist"
+              component={TextField}
+            />
+            <Field
+              label="Discharge date"
+              placeholder="YYYY-MM-DD"
+              name="discharge.date"
+              component={TextField}
+            />
+            <Field
+              label="Discharge criteria"
+              placeholder="Discharge criteria"
+              name="discharge.criteria"
+              component={TextField}
+            />
+            <DiagnosisSelection
+              setFieldValue={setFieldValue}
+              setFieldTouched={setFieldTouched}
+              diagnoses={Object.values(diagnoses)}
+            />
+            <Grid>
+              <Grid item>
+                <Button
+                  color="secondary"
+                  variant="contained"
+                  style={{ float: "left" }}
+                  type="button"
+                  onClick={onCancel}
+                >
+                  Cancel
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  style={{
+                    float: "right",
+                  }}
+                  type="submit"
+                  variant="contained"
+                  disabled={!dirty || !isValid}
+                >
+                  Add
+                </Button>
+              </Grid>
+            </Grid>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default AddEntryForm;
--- part9/patientor-frontend/src/PatientPage/index.tsx
+++ part9/patientor-frontend/src/PatientPage/index.tsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Typography, Container } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
-import { useStateValue, updatePatient } from "../state";
+import { useStateValue, updatePatient, addEntry } from "../state";
 import { apiBaseUrl } from "../constants";
-import { Patient } from "../types";
+import { Patient, Entry } from "../types";
 
 import Entries from "./Entries";
+import AddEntryModal from "../AddEntryModal";
+import { EntryFormValues } from "../AddEntryModal/AddEntryForm";
 
 const PatientPage = () => {
   const { id } = useParams();
   const [{ patients }, dispatch] = useStateValue();
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => {
+    setError(undefined);
+    setModalOpen(false);
+  };
 
   const fetchPatient = async (id: string) => {
     try {
@@ -42,6 +54,29 @@ const PatientPage = () => {
     icon = <span className="material-icons">female</span>;
   }
 
+  const submitNewEntry = async (values: EntryFormValues) => {
+    try {
+      const { data: newEntry } = await axios.post<Entry>(
+        `${apiBaseUrl}/patients/${id}/entries`,
+        values
+      );
+      dispatch(addEntry(id, newEntry));
+      closeModal();
+    } catch (e: unknown) {
+      if (axios.isAxiosError(e)) {
+        console.error(e?.response?.data || "Unrecognized axios error");
+        setError(
+          String(e?.response?.data?.error) || "Unrecognized axios error"
+        );
+      } else {
+        console.error("Unknown error", e);
+        setError("Unknown error");
+      }
+    }
+
+    console.log("Submitting", values);
+  };
+
   return (
     <Container>
       <Typography variant="h4">
@@ -50,6 +85,15 @@ const PatientPage = () => {
       <Typography variant="body1">ssn: {patient.ssn}</Typography>
       <Typography variant="body1">occupation: {patient.occupation}</Typography>
       <Entries entries={patient.entries} />
+      <Button variant="contained" onClick={() => openModal()}>
+        Add New Entry
+      </Button>
+      <AddEntryModal
+        modalOpen={modalOpen}
+        onSubmit={submitNewEntry}
+        error={error}
+        onClose={closeModal}
+      />
     </Container>
   );
 };
